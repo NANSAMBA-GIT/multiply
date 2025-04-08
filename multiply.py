f01f367863034b56533e07d654afdfd90f6037af
olivia <lwangaolivia20@gmail.com>
2025-04-08 20:49:24 +0300

    sucessfulTest

--- multiply.py
+++ multiply.py
@@ -1,8 +1,11 @@
 import unittest
 
+def multiplication(a,b):
+    return a*b
+
 
 class TestMultiply(unittest.TestCase):
-    def test_first(self):
+    def multiply_ones(self):
         self.assertEqual(multiplication(1,1),1)
 
 if __name__ == "__main__":
